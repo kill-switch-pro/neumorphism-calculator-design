59fa35914ef121432266e0f5e6e908ff54967614
Dev S <74428967+kill-switch-pro@users.noreply.github.com>
2023-01-03 09:07:25 +0100

    index.html style.css main.js
    Added all the code for the soft UI calculator design

---
+++ main.js
@@ -0,0 +1,10 @@
+//the line below happen , when you tap the buttons  it get displayed on the screen
+ function cal(dis){
+   document.getElementById("bar").value +=dis;
+ 
+ }
+ 
+ //for the delete (del), button. clears all the input number
+ function del(){
+   document.getElementById("bar").value =" ";
+ }
